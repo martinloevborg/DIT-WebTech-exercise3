d87d9004758fbcee952f5ac798b86f023313cbd7
maloe19 <nartim087@gmail.com>
2023-07-19 22:56:10 +0200

    Beta

---
+++ Pet Shelter/resources/views/adoptions/details.blade.php
@@ -0,0 +1,71 @@
+@extends('master')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">
+                        Adopt me
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title pet-name">{{ $adoption->name }}</h5> <!-- -->
+                        <p class="card-text pet-description">{{ $adoption->description }}</p><!-- -->
+                        <p>Listed by: <b>{{ $adoption->listedBy->name }}</b></p>
+                        <!-- Task 5 User, step 4: this form should not appear if the logged user is giving for adoption this pet -->
+                        <!-- Task 6 User, step 4: this form should not appear if the pet was already adopted -->
+                        
+                        <!-- https://laravel.com/docs/8.x/authorization#via-blade-templates -->
+                        <!-- test for task5: php artisan dusk --filter adoptAnotherUsersPet -->
+                        <!-- test for task6: php artisan dusk --filter showCurrentUserAdoptions -->
+                        @auth
+                        <!--auth -->
+                        @can('adopt', $adoption)
+                        @if(auth()->id() != $adoption->adopted_by)
+                        <!--can('adopt', $adoption)-->
+                        <!--can('mine', $adoption)-->
+                        <!--canany(['adopt', 'mine'], $adoption)-->
+
+                        <!--if(auth()=>id() != $adoption->listed_by && auth()=>id() != $adoption->adopted_by)-->
+                        <!--if(auth()=>id()->canany(['adopt', 'mine'], $adoption))-->
+
+                        <!--if($adoption->listed_by != null) har også prøvet omvendt med id sidst-->
+                        <!--if(auth()->id() !=//== $adoption->listed_by)-->
+                        <!-- -->
+                        <!--if($adoption->adopted_by != null)-->
+                        <!--if(auth()->id() == $adoption->adopted_by)-->
+                        
+                                <form method="post" action="{{ route('adoptions.adopt', [$adoption->id]) }}">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success pet-adopt">Adopt Now
+                                    </button>
+                                </form>
+
+                        <!--endif-->        
+                        <!--endcanany-->
+                        <!--endcan -->
+                        <!--endauth -->
+                        @endif
+                        @endcan
+                        @endauth
+
+                        @if($adoption->adopted_by != null)
+                            @if($adoption->adopted_by == auth()->id())
+                                <p class="text-success">This pet has been adopted by you :)</p>
+                            @else
+                                <p class="text-danger">This pet has already been adopted.</p>
+                            @endif
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="ratio ratio-1x1 ">
+                    <img src="{{ asset($adoption->image_path) }}"
+                         class="card-img-top border border-2 border-dark rounded-3"
+                         style="object-fit: cover" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ Pet Shelter/resources/views/login.blade.php
@@ -0,0 +1,41 @@
+@extends('master')
+
+@section('content')
+    <div
+        style="background-image: url('{{ asset('imgs/login.jpg') }}'); background-size: cover; position: relative"
+        class="h-100">
+        <div style="position: absolute;background-color: black;height: 100%;width: 100%;opacity: 50%"></div>
+        <div class="card shadow-sm"
+             style="width: 500px; top: 50%;left: 50%;  transform: translate(-50%, -50%);position: absolute">
+            <div class="card-header">
+                <h3 class="mb-0">Login</h3>
+            </div>
+            <div class="card-body">
+                <!-- Task 3 Guest, step 5: add the HTTP method and url as instructed-->
+                <form method="POST" action="{{route('doLogin')}}">
+                    @csrf
+                    <!-- Task 3 Guest, step 3: add login fields as instructed-->
+                    <!-- Tip: you can use the same style as the registration form -->
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" name="email" class="form-control email" id="email" value="{{ old('email') }}">
+                        @if($errors->has('email'))
+                            <div class="form-text text-danger">{{ $errors->first('email') }}</div>
+                        @endif
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control password" id="password" value="{{ old('password') }}">
+                        @if($errors->has('password'))
+                            <div class="form-text text-danger">{{ $errors->first('password') }}</div>
+                        @endif
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <!-- Task 3 Guest, step 4: add submit button-->
+                        <input type="submit" class="form-control login-submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
